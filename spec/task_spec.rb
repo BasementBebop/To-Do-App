1a65196da6c5252a99f0230fb2fda62b191dcdcf
David Zulaica & Sean Holstrom <student@epicodus.com>
2015-01-20 14:11:34 -0800

    Refactor Task to communicate with database

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,6 +4,14 @@ require('task')
 
 describe(Task) do
 
+  DB = PG.connect({:dbname => 'to_do_test'})
+
+  RSpec.configure do |config|
+    config.after(:each) do
+      DB.exec("DELETE FROM tasks *;")
+    end
+  end
+
   describe("#description") do
     it("lets you give it a description") do
       test_task = Task.new({:description => "meet gam-gam at the deli for roast beef sammies"})
@@ -25,28 +33,11 @@ describe(Task) do
     end
   end
 
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      test_task = Task.new({:description => "meet gam-gam at the deli for roast beef sammies"})
-      test_task.save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
-    end
-  end
-
   describe("#==") do
     it("is the same task if it has the same description") do
       task1 = Task.new({:description => "learn SQL"})
       task2 = Task.new({:description => "learn SQL"})
       expect(task1).to(eq(task2))
-    end
-  end
-
-  DB = PG.connect({:dbname => 'to_do_test'})
-
-  RSpec.configure do |config|
-    config.after(:each) do
-      DB.exec("DELETE FROM tasks *;")
     end
   end
 
